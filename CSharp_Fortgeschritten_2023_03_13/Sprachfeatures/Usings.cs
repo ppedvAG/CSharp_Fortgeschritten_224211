785ff844b07ff4ab3bf2ea7505b099035f240c0e
ppedvLukasK <lukask@ppedv.de>
2023-03-13 13:46:51 +0100

    Cleanup

--- CSharp_Fortgeschritten_2023_03_13/Sprachfeatures/Usings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Sprachfeatures;
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
--- CSharp_Fortgeschritten_2023_03_13/TopLevelStatement/Program.cs
+++
@@ -1,1 +0,0 @@
-﻿Console.WriteLine(args); //Args Keyword bei Top Level Statement
