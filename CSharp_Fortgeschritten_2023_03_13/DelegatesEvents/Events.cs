256c4896ea3e742b69ea2cb9292a2477d64f4510
ppedvLukasK <lukask@ppedv.de>
2023-03-13 13:42:43 +0100

    M003

---
+++ CSharp_Fortgeschritten_2023_03_13/DelegatesEvents/Events.cs
@@ -0,0 +1,34 @@
+﻿namespace DelegatesEvents;
+
+internal class Events
+{
+	//event: Statischer Punkt an den Methoden angehängt werden können
+	static event EventHandler TestEvent;
+
+	static event EventHandler<TestEventArgs> ArgsEvent;
+		
+	static void Main(string[] args)
+	{
+		TestEvent += Events_TestEvent; //Methode anhängen ohne new, Event kann nicht erstellt werden
+		TestEvent(null, EventArgs.Empty); //Event ausführen
+		TestEvent += (sender, args) => { }; //Anonymes Event anhängen
+
+		ArgsEvent += Events_ArgsEvent;
+		ArgsEvent(null, new TestEventArgs() { Status = "Fertig" });
+	}
+
+	private static void Events_ArgsEvent(object? sender, TestEventArgs e)
+	{
+		Console.WriteLine($"Der Status ist {e.Status}");
+	}
+
+	private static void Events_TestEvent(object? sender, EventArgs e)
+	{
+		Console.WriteLine("TestEvent wurde aufgerufen");
+	}
+}
+
+internal class TestEventArgs : EventArgs
+{
+	public string Status { get; set; }
+}
