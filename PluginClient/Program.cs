731f1f532f15d0efc1ff128d3a0b0f80173a7915
ppedvLukasK <lukask@ppedv.de>
2023-03-15 16:33:34 +0100

    M010

---
+++ PluginClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using PluginBase;
+using System.Reflection;
+
+namespace PluginClient;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		string pfad = @"C:\Users\lk3\source\repos\CSharp_Fortgeschritten_2023_03_13\CalculatorPlugin\bin\Debug\net7.0\Plugin.dll";
+		Assembly loaded = Assembly.LoadFrom(pfad);
+		Type t = loaded.GetTypes().First(e => e.GetInterface("IPlugin") != null);
+		IPlugin plugin = Activator.CreateInstance(t) as IPlugin;
+
+		Console.WriteLine($"Name: {plugin.Name}");
+		Console.WriteLine($"Desc: {plugin.Description}");
+		Console.WriteLine($"Version: {plugin.Version}");
+		Console.WriteLine($"Autor: {plugin.Author}");
+
+		List<MethodInfo> methods = t.GetMethods().Where(e => e.GetCustomAttribute(typeof(ReflectionVisibleAttribute)) != null).ToList();
+		Console.WriteLine("Wähle eine Methode aus");
+		for (int i = 0; i < methods.Count; i++)
+		{
+			Console.WriteLine($"{i}: {methods[i].Name}");
+		}
+
+		int auswahl = int.Parse(Console.ReadLine());
+		Console.WriteLine(methods[auswahl].Invoke(plugin, new object[] { 2.2, 3.3 }));
+	}
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+
+namespace Reflection;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Program p = new();
+		Type pt = p.GetType(); //Typ holen mit GetType() über Objekt
+		Type t = typeof(Program); //Typ holen durch typeof(<Klassenname>)
+
+		object o = Activator.CreateInstance(pt); //Objekt über Typ erstellen
+
+		pt.GetMethods(); //Alle Methoden anschauen
+		pt.GetMethod("Test").Invoke(o, null); //Methode über Reflection aufrufen, braucht ein Objekt des entsprechenden Typs
+		pt.GetMethod("Test2").Invoke(o, new[] { "Zwei Test" }); //Methode mit Parameter aufrufen
+
+		pt.GetField("Zahl").SetValue(o, 5); //Feld setzen
+		Console.WriteLine((int) pt.GetField("Zahl").GetValue(o)); //Wert holen aus Feld
+
+		/////////////////////////////////////////////////
+
+		object o2 = Activator.CreateInstance("Reflection", "Reflection.Program"); //Objekt nur über Strings erstellen
+
+		Assembly assembly = Assembly.GetExecutingAssembly(); //Information über das derzeitige Projekt erhalten
+
+		assembly.GetTypes(); //Alle Typen des Assemblies holen
+
+		assembly.GetType("Program"); //einzelnen Typen finden
+
+		//object o3 = Activator.CreateInstance(assembly.GetType("Program"));
+
+		string pfad = @"C:\Users\lk3\source\repos\CSharp_Fortgeschritten_2023_03_13\DelegatesEvents\bin\Debug\net7.0\DelegatesEvents.dll";
+
+		Assembly loaded = Assembly.LoadFrom(pfad); //DLL laden
+
+		Type loadedType = loaded.GetType("DelegatesEvents.Component");
+
+		object comp = Activator.CreateInstance(loadedType);
+		loadedType.GetEvent("ProcessCompleted").AddEventHandler(comp, () => Console.WriteLine("Prozess fertig"));
+		loadedType.GetEvent("Progress").AddEventHandler(comp, (int i) => Console.WriteLine($"Fortschritt: {i}"));
+		loadedType.GetMethod("StartProcess").Invoke(comp, null);
+	}
+
+	public int Zahl;
+
+	public void Test()
+	{
+		Console.WriteLine("Ein Test");
+	}
+
+	public void Test2(string x)
+	{
+		Console.WriteLine(x);
+	}
+}
